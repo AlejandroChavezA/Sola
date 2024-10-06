6d8c7fe099938f5b7b0146985a731538783008e0
dami <129913891+dami0910@users.noreply.github.com>
2024-10-06 09:46:41 -0600

    fix branch

--- WhatAreSolarStorms.jsx
+++ WhatAreSolarStorms.jsx
@@ -4,38 +4,6 @@ import './WhatAreSolarStorms.css';
 const WhatAreSolarStorms = forwardRef((props, ref) => {
   return (
     <>
-<<<<<<< HEAD
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">Sola</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/info">Info</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something else here</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <section ref = {ref} id = "what-are-solar-storms" className = "content">
-        <h2>¿Qué son las tormentas solares?</h2>
-        <p>
-        Son tormentas causadas por “tormentas solares”, esas tormentas causan 
-        perturbaciones mayores al campo magnético de la Tierra. Todo esto produce efectos 
-        como interrupciones en la radio, apagones de energía,  y muy hermosas auroras.
-        </p>
-
-        h
-        {props.children}
-      </section>
-=======
       {/* <nav className="navbar">
         <div className="nav-link">Home</div>
         <div className="nav-link">Info</div>
@@ -56,7 +24,6 @@ const WhatAreSolarStorms = forwardRef((props, ref) => {
       
         
 
->>>>>>> 192e1a9 (1,3)
     </>
   );
 });
